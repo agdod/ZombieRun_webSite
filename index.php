c9361154a28b597c26f6e2736ca7d43a7ecb75f6
agdod <adodson1510@hotmail.com>
2019-11-24 18:19:24 +0200

    rename ids and a few class

--- index.php
+++ index.php
@@ -13,46 +13,52 @@
     <body>
         <?php include ('navbar.php') ?> 
         <div id="layout">
-                    <div id="header"><h1>ZombieRun</h1></div> 
-                    <div id="main">
-                        <div id="main_content">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus felis orci, euismod et dapibus sed, vestibulum sed nibh. 
-                        Sed consectetur odio nulla. Nam at magna imperdiet, viverra neque sit amet, ultrices libero. Praesent consequat hendrerit tellus sed scelerisque. 
-                        Nunc gravida tellus a lacus maximus tristique gravida at nunc. In hac habitasse platea dictumst. Praesent blandit dolor ac accumsan consectetur. 
-                        Praesent blandit elit vulputate lacus laoreet ultricies. Fusce scelerisque nunc vitae quam scelerisque, sed aliquam lectus condimentum. 
-                        Donec dignissim sed tellus at volutpat. Praesent viverra fermentum ligula a commodo. Praesent imperdiet in odio in pellentesque. 
-                        </div>
-                        <div id="main_image">
-                            <img src="/img/apocalipse.png" alt="Post world apocalipse">
-                        </div>   
+            <div id="header">
+                <h1>ZombieRun</h1>
+            </div> 
+            <div id="main">
+                <div class="content"> 
+                    <div class="text_content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus felis orci, euismod et dapibus sed, vestibulum sed nibh. 
+                    Sed consectetur odio nulla. Nam at magna imperdiet, viverra neque sit amet, ultrices libero. Praesent consequat hendrerit tellus sed scelerisque. 
+                    Nunc gravida tellus a lacus maximus tristique gravida at nunc. In hac habitasse platea dictumst. Praesent blandit dolor ac accumsan consectetur. 
+                    Praesent blandit elit vulputate lacus laoreet ultricies. Fusce scelerisque nunc vitae quam scelerisque, sed aliquam lectus condimentum. 
+                    Donec dignissim sed tellus at volutpat. Praesent viverra fermentum ligula a commodo. Praesent imperdiet in odio in pellentesque. 
                     </div>
-                    <div id="subSectionAds">
-                        <div class="ads"> <!-- use class id here as ads could be placed on several areas of the age/ site and set up using a single class -->  
-                            <!-- ?php include('ads.php') --> <!-- single file for ads allowing modifation to one file
-                                would allow for a dynmic website althoguh could put more load on HTML requests, accessing for many php files.logo
-                                Although if files are in many pages it will speed up time as file will be cached.  -->
-                                for now jsut do a single flexigrid 1 colum many rows list of "ads"
-                        </div>
+                    <div class="img_content">
+                        <img src="/img/apocalipse.png" alt="Post world apocalipse">
+                    </div>   
+                </div>
+            </div> <!-- end of div "main" -->
+            <div id="subSectionAds">
+                <div class="ads"> <!-- use class id here as ads could be placed on several areas of the age/ site and set up using a single class -->  
+                    <!-- ?php include('ads.php') --> <!-- single file for ads allowing modifation to one file
+                        would allow for a dynmic website althoguh could put more load on HTML requests, accessing for many php files.logo
+                        Although if files are in many pages it will speed up time as file will be cached.  -->
+                        for now jsut do a single flexigrid 1 colum many rows list of "ads"
+                </div>
+            </div>
+            <div id="subSectionExtra">
+                <div class="content">
+                    <div class="text_content">
+                        Zombie run is a third person view set ina  post apolicatic world. <br>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
                     </div>
-                    <div id="subSectionExtra">
-                        <div id="extra_content">
-                            Zombie run is a third person view set ina  post apolicatic world. <br>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                        </div>
-                        <div id="extra_images">
-                            <img src="/img/mainCharacter.png" alt text=" main character">
-                        </div>
+                    <div class="img_content">
+                        <img src="/img/mainCharacter.png" alt text=" main character">
                     </div>
-                    <div id="SubSectionLinks">
-                        <?php include('common_links.php') ?>
-                        <!-- extra links can be added here for indivdual pages -->                                                
-                    </div>
-                    <div id="footer">
-                       <?php include('footer.php') ?> 
-                    </div>
+                </div>
+            </div>
+            <div id="SubSectionLinks">
+                <?php include('common_links.php') ?>
+                <!-- extra links can be added here for indivdual pages -->                                                
+            </div>
+            <div id="footer">
+                <?php include('footer.php') ?> 
+            </div>
         </div>
         
        
