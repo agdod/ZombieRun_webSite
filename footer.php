bb1fcf84e335b6873510e59fd5d532af9397d3e9
agdod <adodson1510@hotmail.com>
2019-11-24 12:50:29 +0200

    footer added

---
+++ footer.php
@@ -0,0 +1,5 @@
+<div id="footer_contents">
+        ZombieRun - website and game develpment Desgined & coded by Andy.G 2019 as part of the <a href="https://www.h2c.fi">H2C programm</a> 
+        in conjunctaion with <a href="https://www.turkuamk.fi">TurkuAMK</a>
+          
+</div>
--- index.php
+++ index.php
@@ -13,15 +13,17 @@
     <body>
         <?php include ('navbar.php') ?> 
         <div id="layout">
-                   <div id="header"><h1>ZombieRun</h1></div> 
-                   <div id="main">MAIN</div>
-                   <div id="subSectionAds">ADS</div>
-                   <div id="subSectionExtra">EXTRA</div>
-                   <div id="SubSectionLinks">LINKS</div>
-                   <div id="footer">FOOTER</div>
+                    <div id="header"><h1>ZombieRun</h1></div> 
+                    <div id="main">MAIN</div>
+                    <div id="subSectionAds">ADS</div>
+                    <div id="subSectionExtra">EXTRA</div>
+                    <div id="SubSectionLinks">LINKS</div>
+                    <div id="footer">
+                       <?php include('footer.php') ?> 
+                    </div>
         </div>
         
-       <!--?php include('footer.php') ?> footer same as nav bar-->  
+       
        <!-- bootstrap javascript files -->
        <!--script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
