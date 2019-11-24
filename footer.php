b8f144aceb0ef506759ac2a0b267b639cd57b86a
agdod <adodson1510@hotmail.com>
2019-11-24 14:43:21 +0200

    comment aded for futeuer refernce

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="footer_contents">
         ZombieRun - website and game develpment Desgined & coded by Andy.G 2019 as part of the <a href="https://www.h2c.fi">H2C programm</a> 
         in conjunctaion with <a href="https://www.turkuamk.fi">TurkuAMK</a>
-          
+       <!-- do full screen up to 875 px then do a shorter text for mobile /tablet wiht drop down on hover -->   
 </div>
