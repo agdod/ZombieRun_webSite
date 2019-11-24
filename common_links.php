40eba126fdd96785be2e072ee92729d4eab636c8
agdod <adodson1510@hotmail.com>
2019-11-24 14:41:41 +0200

    added link file

---
+++ common_links.php
@@ -0,0 +1,9 @@
+<div class="links">
+    <p>
+        <a href="https://www.H2C.fi"> Highway2Code </a> <br>
+        <a href="https://www.turkuamk.fi"> TurkuAmk </a> <br>
+        <a href="https://www.turkugamelab.fi"> Turku Game Lab </a> <br>
+        <a href="https://www.unity.com"> Unity Game Develpment </a> <br>
+        <a href="https://www.w3schools.com">W3Schools </a> <br>
+    </p>
+</div>
