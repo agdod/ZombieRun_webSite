897f844dfc2047312cd678e8b26bc9ea9cc3eb52
agdod <adodson1510@hotmail.com>
2019-12-08 08:45:21 +0200

    added "viewport" and tidy up to header

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, inital-scale2!">
-        <title>ZombieRun</title>
+        <meta name="viewport" content="width=device-width, inital-scale=1.0">
+        
         <link rel="stylesheet" type="text/css" href="/css/main.css">
         <!-- bootstrap css -->
         <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"-->
+        <title>ZombieRun</title>
     </head>
 
     <body>
