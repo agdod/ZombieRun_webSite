abefe0c6b4b4ee869d0dadefd2dffef2edd72dd6
agdod <adodson1510@hotmail.com>
2019-12-09 13:17:20 +0200

    tidy up

--- index.php
+++ index.php
@@ -4,16 +4,13 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, inital-scale=1.0">
-        
-        <link rel="stylesheet" type="text/css" href="/css/main-test.css">
-        <!-- bootstrap css -->
-        <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"-->
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
         <title>ZombieRun</title>
     </head>
 
     <body>
-            <?php include ('navbar.php') ?> 
-            <div class="grid-container" >
+        <?php include ('navbar.php') ?> 
+        <div class="grid-container" >
             <div class="col-12 header">
                 <h1>ZombieRun</h1>
             </div> 
@@ -23,9 +20,7 @@
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-                <div class="m-img">
-                    <img src="/img/apocalipse.png" alt="Post world apocalipse">
-                </div>
+                <img src="/img/apocalipse.png" alt="Post world apocalipse">
             </div>
             <div class="col-6 main">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus felis orci, euismod et dapibus sed, vestibulum sed nibh. 
@@ -33,31 +28,26 @@
                 Nunc gravida tellus a lacus maximus tristique gravida at nunc. In hac habitasse platea dictumst. Praesent blandit dolor ac accumsan consectetur. 
                 Praesent blandit elit vulputate lacus laoreet ultricies. Fusce scelerisque nunc vitae quam scelerisque, sed aliquam lectus condimentum. 
                 Donec dignissim sed tellus at volutpat. Praesent viverra fermentum ligula a commodo. Praesent imperdiet in odio in pellentesque. 
-                    <div class="m-img">
-                        <img src="/img/mainCharacter.png" alt text=" main character">    
-                    </div> 
+                <img src="/img/mainCharacter.png" alt text=" main character">    
+                    
             </div>  
             <div class="col-3 left ads">
-                <!-- use class id here as ads could be placed on several areas of the age/ site and set up using a single class -->  
-            <!-- ?php include('ads.php') --> <!-- single file for ads allowing modifation to one file
-            would allow for a dynmic website althoguh could put more load on HTML requests, accessing for many php files.logo
-            Although if files are in many pages it will speed up time as file will be cached.  -->
-            for now jsut do a single flexigrid 1 colum many rows list of "ads"
-                <div class="links">
-                    <?php include('common_links.php') ?>
-                    <!-- extra links can be added here for indivdual pages -->                                                
-                </div>
+                <p>
+                    <!-- use class id here as ads could be placed on several areas of the age/ site and set up using a single class -->  
+                    <!-- ?php include('ads.php') --> <!-- single file for ads allowing modifation to one file
+                    would allow for a dynmic website althoguh could put more load on HTML requests, accessing for many php files.logo
+                    Although if files are in many pages it will speed up time as file will be cached.  -->
+                    for now jsut do a single flexigrid 1 colum many rows list of "ads"
+                    <div class="links">
+                        <?php include('common_links.php') ?>
+                        <!-- extra links can be added here for indivdual pages -->                                                
+                    </div>
+                </p>
+                
             </div>
             <div class="col-12 footer">
-                    <?php include('footer.php') ?> 
+                <?php include('footer.php') ?> 
             </div>
         </div>
-        
-        
-       
-       <!-- bootstrap javascript files -->
-       <!--script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script-->
     </body>
 </html>
