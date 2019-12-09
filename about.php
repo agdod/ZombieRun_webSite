2cb340fcc2a241793608a1e97bdd54f0b2eacb98
agdod <adodson1510@hotmail.com>
2019-12-09 22:56:12 +0200

    some about content added

--- about.php
+++ about.php
@@ -19,13 +19,23 @@
             <div class="col-6 main">
                     <h2>The game objective</h2>
                     <!-- Game screen shot or two here-->
+                    Zombie Run is a third person infinte runner. The objective o the game is to survive. Avoid the hoard and stay alive for as long as you can.
+                    
+                    Background 
+                    Set in post apolcityc world where a zombie virus has ranveged mankind.logo Handfull of surviors are scattered across teh glode.
+                    Youve heard that in the north there isnt zombies zombies dont like and cant stand your cold.You decide to head north.
+                    
+                    Variuos scene to test your skills on, dodge slide and jump the obsticles and dodge the zombie, blast them on your way. be warned they are attarcted to noise though.
+                    Collect the power up and health packs to help you progress.
+                    Exchange you gained loot for additial goodies at the tading posts to help you on your way
+
                     infintiy runner 3rd person style. Dodge attack the zombies SURVIVE. 
-                    Numerous levels of iniftie fun, dodge attacking the zombies, get power up and hiddens weapons. Find the secerat areas. Do you have what it take to survive.logo
-                
-                    Set in post apolcityc world where a zombie virus has ranveged mankind.logo Handfull of surviors are scattered across teh glode.logo
-                    Youve heard that in the north there isnt zombies zombies dont like and cant stand your cold.You decide to head north. 
+                    Numerous levels of iniftie fun, dodge attacking the zombies, get power up and hiddens weapons. Find the secerat areas. Do you have what it take to survive.
                     
                     Your journey begins
+
+
+                    
             </div>  
             <div class="col-3 left ads">
                 <h2>About myself</h2>
@@ -35,7 +45,11 @@
                 </p>
                 <h2>About this project</h2>
                 <p>
-                    This site and the game where dont as part of the <a href="https://h2c.fi/">Highway2Code</a>trainign programme. Undertacken in conjuntion wiht TurkAmk and Turku GameLab
+                Zombie run was desgined and developed as part of the Highway to code leanring programme. teaching coding and basic game mecanics a quick learning curve of both unity game engine
+                    game develpment and c# was made during this course. the outcome a "reasonable" playable game. 
+                    With time ad  develpment hopfully the game can develp more and further imporve - along wiht my coding skill set.    
+                
+                    This site was also develped along wiht  the game where dont as part of the <a href="https://h2c.fi/">Highway2Code</a>trainign programme. Undertacken in conjuntion wiht TurkAmk and Turku GameLab
                 </p>
             </div>
             <div class="col-12 footer solid-divider">
