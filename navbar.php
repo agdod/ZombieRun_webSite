063ee14b638a5aa8bcf63da0c11bfe1b24075a5c
agdod <adodson1510@hotmail.com>
2019-12-09 13:10:39 +0200

    --

--- navbar.php
+++ navbar.php
@@ -1,15 +1,13 @@
-<div >
-    <div class="col-12 navbar">
-        <ul> 
-            <li class="logo"><img src="/img/logo_ZombieRun.png" alt="ZombieRunLogo"></li>
-            <li> <a href="index.php">Home</a> </li>
-            <li> <a href="about.php">About</a> </li>
-            <li> <a href="contact.php">Contact</a> </li>
-            <li> <a href="login.html">SignUp / Login</a> </li>   <!-- Signup login page is same page use php script to detect if logged in, if true logout 
-                                                                    if false display login, with link to signup php script
-                                                                    PHP script used to detect if signed in if true this link is displayed -->
+<div class="col-12 navbar">
+    <ul> 
+        <li class="logo"><img src="/img/logo_ZombieRun.png" alt="ZombieRunLogo"></li>
+        <li> <a href="index.php">Home</a> </li>
+        <li> <a href="about.php">About</a> </li>
+        <li> <a href="contact.php">Contact</a> </li>
+        <li> <a href="login.html">SignUp / Login</a> </li>   <!-- Signup login page is same page use php script to detect if logged in, if true logout 
+                                                                if false display login, with link to signup php script
+                                                                PHP script used to detect if signed in if true this link is displayed -->
 
-            <li> <a href="userProfile.html"> Profile </a> </li> 
-        </ul> 
-    </div>
+        <li> <a href="userProfile.html"> Profile </a> </li> 
+    </ul> 
 </div>
