8c0bd70834823039e092a34c9d03d39fb85b2bd7
agdod <adodson1510@hotmail.com>
2019-12-10 08:01:28 +0200

    added hamburger menu navbar

--- navbar.php
+++ navbar.php
@@ -1,13 +1,15 @@
-<div class="col-12 navbar">
+<div class="navbar" id="myNavbar">
     <ul> 
         <li class="logo">ZOMBIERUN</li>
         <li> <a href="index.php">Home</a> </li>
         <li> <a href="about.php">About</a> </li>
         <li> <a href="contact.php">Contact</a> </li>
-        <li> <a href="login.html">SignUp / Login</a> </li>   <!-- Signup login page is same page use php script to detect if logged in, if true logout 
+        <li> <a href="login.html">Login</a> </li>   <!-- Signup login page is same page use php script to detect if logged in, if true logout 
                                                                 if false display login, with link to signup php script
                                                                 PHP script used to detect if signed in if true this link is displayed -->
 
         <li> <a href="userProfile.html"> Profile </a> </li> 
+        <li> <a href="javascript:void(0);" class="icon" onclick="navbarFunction()">
+            <i class="fa fa-bars"></i></a></li>
     </ul> 
 </div>
