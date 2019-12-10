a0abd39f72116b0efea7578a776669400a46bd20
agdod <adodson1510@hotmail.com>
2019-12-10 08:00:34 +0200

    redundency removed

--- about.php
+++ about.php
@@ -6,38 +6,35 @@
         <link rel="stylesheet" type="text/css" href="/css/main.css">
         <title>About</title>
     </head>
-    <!-- include navbar php file-->
+    
     <body>
         <?php include ('navbar.php') ?> 
         <div class="grid-container" >
-            <div class="col-12 header">
+            <div class="header">
                 <h1>About</h1>
             </div> 
-            <div class="col-3 right">
+            <div class="right">
                 right colume content
             </div>
-            <div class="col-6 main">
-                    <h2>The game objective</h2>
-                    <!-- Game screen shot or two here-->
-                    Zombie Run is a third person infinte runner. The objective o the game is to survive. Avoid the hoard and stay alive for as long as you can.
-                    
-                    Background 
-                    Set in post apolcityc world where a zombie virus has ranveged mankind.logo Handfull of surviors are scattered across teh glode.
-                    Youve heard that in the north there isnt zombies zombies dont like and cant stand your cold.You decide to head north.
-                    
-                    Variuos scene to test your skills on, dodge slide and jump the obsticles and dodge the zombie, blast them on your way. be warned they are attarcted to noise though.
-                    Collect the power up and health packs to help you progress.
-                    Exchange you gained loot for additial goodies at the tading posts to help you on your way
+            <div class="main">
+                <h2>The game objective</h2>
+                <!-- Game screen shot or two here-->
+                Zombie Run is a third person infinte runner. The objective o the game is to survive. Avoid the hoard and stay alive for as long as you can.
+                
+                Background 
+                Set in post apolcityc world where a zombie virus has ranveged mankind.logo Handfull of surviors are scattered across teh glode.
+                Youve heard that in the north there isnt zombies zombies dont like and cant stand your cold.You decide to head north.
+                
+                Variuos scene to test your skills on, dodge slide and jump the obsticles and dodge the zombie, blast them on your way. be warned they are attarcted to noise though.
+                Collect the power up and health packs to help you progress.
+                Exchange you gained loot for additial goodies at the tading posts to help you on your way
 
-                    infintiy runner 3rd person style. Dodge attack the zombies SURVIVE. 
-                    Numerous levels of iniftie fun, dodge attacking the zombies, get power up and hiddens weapons. Find the secerat areas. Do you have what it take to survive.
-                    
-                    Your journey begins
-
-
-                    
+                infintiy runner 3rd person style. Dodge attack the zombies SURVIVE. 
+                Numerous levels of iniftie fun, dodge attacking the zombies, get power up and hiddens weapons. Find the secerat areas. Do you have what it take to survive.
+                
+                Your journey begins        
             </div>  
-            <div class="col-3 left ads">
+            <div class="left ads">
                 <h2>About myself</h2>
                 <p>
                     budding competer programmer living in turku finland
