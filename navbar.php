1257846b14bf67028ff1546459342605ffc5c9ff
agdod <adodson1510@hotmail.com>
2019-12-10 20:30:51 +0200

    added responvie navbar and div

--- navbar.php
+++ navbar.php
@@ -1,9 +1,6 @@
 <div class="navbar" id="myNavbar">
-     
-        <div class="logo col-25">ZOMBIERUN</li> </div>
-        <div class="row">
-        <div class="col-75">
-        <ul >
+    <ul> 
+        <li class="logo">ZOMBIERUN</li>
         <li> <a href="index.php">Home</a> </li>
         <li> <a href="about.php">About</a> </li>
         <li> <a href="contact.php">Contact</a> </li>
@@ -12,11 +9,20 @@
                                                                 PHP script used to detect if signed in if true this link is displayed -->
 
         <li> <a href="userProfile.html" > Profile </a> </li> 
-</ul>
+        </ul>
+        <div class="navbar-s">
+         <a href="index.php">Home</a> 
+         <a href="about.php">About</a> 
+         <a href="contact.php">Contact</a> 
+         <a href="login.html">Login</a>    <!-- Signup login page is same page use php script to detect if logged in, if true logout 
+                                                                if false display login, with link to signup php script
+                                                                PHP script used to detect if signed in if true this link is displayed -->
+
+        <a href="userProfile.html" > Profile </a>
         <a href="javascript:void(0);" class="icon" onclick="navbarFunction()">
             <i class="fas fa-bars"></i></a>
-        
-        </div>
 </div>
+    
+    
     
 </div>
