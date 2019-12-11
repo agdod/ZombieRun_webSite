bafdcc54ca4027016c9330b10ba41d6664206d9a
agdod <adodson1510@hotmail.com>
2019-12-11 18:57:51 +0200

    adding 404 page

--- 404.php
+++ 404.php
@@ -8,29 +8,32 @@
     <style>
         body {
             /** different size images for different size displays need to be re sized**/
-            background-image: url("/img/apocalipse-800x450.png");
+            background-image: url("/img/apocalipse.png");
             background-repeat: no-repeat;
             /*background-position: center center ;*/
             background-size: cover;
         }
     </style> 
-    <title>Error Not found </title>
+    <title>Error Page Not found </title>
     </head>
     <body>
-        <?php include ('navbar.php') ?>     
-            <div>
-            
-            <p><b>Sorry but the page you was lookoing for was not found on this site.</b><br>
-                Return to the <a href="index.html">home page</a> or contact the Admin of the site</a><p></p>
-                
-                
-            <br>
-            <br>
-            <p style="text-align:center;">Have a nice day.</p>
-        </div>
-        <div class="col-12 footer">
+        <?php include ('navbar.php') ?> 
+        <div class="grid-container" >
+            <div class="col-12 header">
+                <h1>ZombieRun</h1>
+            </div> 
+            <div class="main notfound">
+                <p> 
+                    <b>Sorry but the page you was looking for was not found on this site.</b><br>
+                    Return to the <a href="index.php">home page</a> or contact the Admin of the site</a>
+                </p>
+                <br><br>
+                <p>Have a nice day.</p>
+            </div>  
+            <div class="footer">
                     <?php include('footer.php') ?> 
             </div>
-    </body>
+        </div>
+    </body>            
 
 </html>
