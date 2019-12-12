49483c6634d9b8afbdf2ccc8706e4bd0b63ebe46
agdod <adodson1510@hotmail.com>
2019-12-12 10:21:30 +0200

    file name changed to zombieun.js

--- commonscripts.php
+++ commonscripts.php
@@ -1,3 +1,3 @@
 <!-- addjavascript files here -->
 <script src="https://kit.fontawesome.com/0f60ab991b.js" crossorigin="anonymous"></script> <!-- for using awesome fonts icon -->
-<script src="/src/ZR-navbar.js"></script>
+<script src="/src/zombierun.js"></script>
