d5c2e10f3c98b578c8512d461d4462c2ba90d671
agdod <adodson1510@hotmail.com>
2019-12-12 10:20:29 +0200

    renamed

--- src/ZR-navbar.js
+++
@@ -1,8 +0,0 @@
-function navbarFunction(){
-    let x=document.getElementById("myNavbar");
-    if (x.className === "navbar"){
-        x.className +="-responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
