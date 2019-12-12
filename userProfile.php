5702927c7539b5d600e1a74cc6edfb10ff6e9556
agdod <adodson1510@hotmail.com>
2019-12-12 08:20:21 +0200

    bit of content

---
+++ userProfile.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, inital-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="/css/main.css">
+        <title>page title goes here</title>
+    
+    </head>
+                    
+    <body>
+        <?php include ('navbar.php') ?> 
+        <div class="grid-container" >
+            <div class="header">
+                <h1>User Profile</h1>
+            </div> 
+            <div class="right">
+                other links to more profile related stuff
+            </div>
+            <div class="main">
+                Welcome here will be displayed information on the user stats 
+                <p>
+                    user image/avater place holders and all 
+                </p>
+            </div>  
+            <div class="left ads">
+                list of equipment bought
+                <br>
+                few adds 
+                <br>
+                some links
+
+            </div>
+            <div class="footer">
+                    <?php include('footer.php') ?> 
+            </div>
+        </div>
+    </body>            
+</html>
