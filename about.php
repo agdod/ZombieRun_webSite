4b9663ce6130ec2c587bea10f8fa79ae2ea163cf
agdod <adodson1510@hotmail.com>
2019-12-13 10:28:55 +0200

    added include link to links file

--- about.php
+++ about.php
@@ -25,11 +25,7 @@
                 Undertacken in conjuntion with <a href="https://www.turkuamk.fi">TurkAmk</a> and Turku GameLab
                 </p>
                 <hr>
-                <div class="links">
-                    <a href="https://www.h2c.com">H2C - learning program</a><br>
-                    <a href="https://www.turkuamk.fi">TurkuAMK</a><br>
-                    <a href="https://www.unity.com">Unity</a><br>
-                </div>
+                <?php include('links.php') ?>
             </div>
             <div class="main">
                 <h2>The game objective</h2>
@@ -47,55 +43,59 @@
                 infintiy runner 3rd person style. Dodge attack the zombies SURVIVE. 
                 Numerous levels of iniftie fun, dodge attacking the zombies, get power up and hiddens weapons. Find the secerat areas. Do you have what it take to survive.
                 
-                Your journey begins    
-                <h3> Screen shots </h3>
-                <div class="row">
-                    <div class="col-70 col-a-r">
-                        <img src="/img/opening sequence.jpg" alt="Opening squence">Opening sequence. 
-                        Camera pans slowly over a half destroyed bridge.
-                    </div>
-                    <div class="col-25 col-a-l">
-                        <img src="/img/mainCharacter.png" alt="main character"> Main Character
-                        <i>still to decide on name...!</i>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-70">
-                        <img src="/img/startoff-w-swinging.jpg" alt="swinging weapon"> Swinging the buldgen weapon
-                    </div>
-                    <div class="col-25 t-a-right">
-                        The start off point a trail swing before setting off  
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-70 col-a-r" >
-                        <img src="/img/screenshot.jpg" alt="attacked by zombies"> in the think of it
-                    </div>
-                    <div class="col-25 col-a-l">
-                        <img src="/img/mainmenu.jpg" alt="main menu">Screen shot of main menu
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-25 col-a-r">
-                        
-                    </div>
-                    <div class="col-75 ">
-                         
-                    </div>
-                </div>
+                Your journey begins 
+                <h3> Screen shots </h3>   <span id="dots">....</span>
+                <div class="more" id="moreBtn" onclick="ShowMore()">Read more...</div>
                 
+                <span id="more">
+                    <div class="row">
+                        <div class="col-70 col-a-r">
+                            <img src="/img/opening sequence.jpg" alt="Opening squence">Opening sequence. 
+                            Camera pans slowly over a half destroyed bridge.
+                        </div>
+                        <div class="col-25 col-a-l">
+                            <img src="/img/mainCharacter.png" alt="main character"> Main Character
+                            <i>still to decide on name...!</i>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-70">
+                            <img src="/img/startoff-w-swinging.jpg" alt="swinging weapon"> Swinging the buldgen weapon
+                        </div>
+                        <div class="col-25 t-a-right">
+                            The start off point a trail swing before setting off  
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-70 col-a-r" >
+                            <img src="/img/screenshot.jpg" alt="attacked by zombies"> in the think of it
+                        </div>
+                        <div class="col-25 col-a-l">
+                            <img src="/img/mainmenu.jpg" alt="main menu">Screen shot of main menu
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-25 col-a-r">
+                            
+                        </div>
+                        <div class="col-75 ">
+                            
+                        </div>
+                    </div>
+                </span>
             </div>  
             <div class="left ads">
-                <h2>About myself</h2>
-                <div class="col-70">
-                    <p>
-                    Aspired computer programmer.
-                    <!--just some extra filler text -->
-                    </p>
-                </div>
-                <div class="col-30 a-right">
-                    <img src="" alt="my image" >
-                </div>
+                <h2> About Me : - </h2>
+                <div class="card">
+                    <img src="/img/img_avatar.png" alt="Avatar" style="width:100%">
+                    <div class="container">
+                        <h4><b>Andy Dodson</b></h4>
+                        <p>Web Develper</p>
+                        <p>
+                            <!-- add some text here-->
+                        </p>
+                    </div>
+                </div>  
             </div>
             <div class="col-12 footer solid-divider">
                     <?php include('footer.php') ?> 
