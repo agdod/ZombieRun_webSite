05bf1ed244f186716eced1a0ed7afccf09ebf16f
agdod <adodson1510@hotmail.com>
2019-12-13 10:29:33 +0200

    added content

--- contact.php
+++ contact.php
@@ -16,9 +16,30 @@
                 <h1>Contact Us:</h1>
             </div> 
             <div class="right">
-               right colume content
-            </div>
+                <h2>Ads :</h2>
+                <div class="card ">
+                        <img src="/img/img_avatar.png" alt="Avatar" style="width:60%">
+                        <div class="container">
+                            <h4><b>Elleros Games</b></h4>
+                            <p>game develpement enterpises</p>
+                            <p>
+                                leading game develpment 
+                            </p>
+                        </div>
+                    </div> 
+                    <div class="card">
+                        <img src="/img/img_avatar.png" alt="Avatar" style="width:60%">
+                        <div class="container">
+                            <h4><b>Elleros Enterpise </b></h4>
+                            <p>Web Develpment spiecalist</p>
+                            <p>
+                                for all you web devlpment needs
+                            </p>
+                        </div>
+                    </div> 
+            </div>  <!--end of right-->
             <div class="main">
+            Contact us. We'd love to hear from you! Send us your thoughts, ideas, suggestions, comments, feedback, or  even write a review. 
                 <form action="action_page.php">
                     <div class="row">
                         <div class="col-25">
@@ -62,9 +83,57 @@
                         <input type="submit" value="Submit">
                     </div>
                 </form>
+                <hr>
+                <h4>Meet the team: </h4>
+                <div class="row"> 
+                    <div class="col-33">
+                        <div class="card">
+                            <img src="/img/img_avatar.png" alt="Joe Bloggs" style="width:80%">
+                            <div class="container">
+                                <h4><b>Joe blogs</b></h4>
+                                <p class="title">Customer Service</p>
+                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                <p>joebloggs@ellerosgames.test</p>
+                                <p><button class="button-gen">Contact</button></p>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="col-33">
+                        <div class="card">
+                            <img src="/img/img_avatar.png" alt="John Doe" style="width:80%">
+                            <div class="container">
+                                <h4><b>John Doe</b></h4>
+                                <p class="title">Customer Service</p>
+                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                <p>johndoe@ellerosgames.test</p>
+                                <p><button class="button-gen">Contact</button></p>
+                            </div>
+                        </div> 
+                    </div>
+                    <div class="col-33">
+                        <div class="card">
+                            <img src="/img/img_avatar2.png" alt="Jane Doe" style="width:80%">
+                            <div class="container">
+                                <h4><b>Jane Doe</b></h4>
+                                <p>Senior Web Develpor </p>
+                                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                                <p>janedoe@ellerosgames.test</p>
+                                <p><button class="button-gen">Contact</button></p>
+                            </div>
+                        </div> 
+                    </div> 
+                </div> <!-- end of meet the team-->
             </div>
             <div class="left ads">
-                left columen content
+                <!--?php include('links.php') ?-->
+                # some filler text <br>
+                here will be adds space <br>
+                and some links too <br>
+                ad1 <br> ad2<br> ...etc<br>
+                <hr>
+                <div class="links">
+                    <?php include('links.php') ?>
+                </div>    
             </div>
             <div class="footer solid-divider">
                     <?php include('footer.php') ?> 
